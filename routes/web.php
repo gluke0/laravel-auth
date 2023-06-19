5e79b90a0173130482688376154fd3acdb95d5f2
gluke0 <milanesigianluca@gmail.com>
2023-06-19 17:06:11 +0200

    prefix 'admin' in the midd route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function (){
 });
 
 // profile routes, middleware handles all these routes 
-Route::middleware(['auth', 'verified'])->group(function (){
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function(){
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
