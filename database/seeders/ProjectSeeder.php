d07cc73dbb4714f9a77b44eb766ba51ba7e51c4d
gluke0 <milanesigianluca@gmail.com>
2023-06-20 14:56:29 +0200

    db seeded with faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker; 
+use App\Models\Admin\Project; 
+
+// library to use Laravel Helper
+use Illuminate\Support\Str; 
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +17,16 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(2);
+            $newProject->slug = Str::slug($newProject->title, '-');
+            $newProject->description = $faker->text(300);
+            $newProject->languages = $faker->sentence(3);
+            $newProject->image = $faker->image(null, 640, 480);
+            $newProject->save();
+        }
     }
 }
